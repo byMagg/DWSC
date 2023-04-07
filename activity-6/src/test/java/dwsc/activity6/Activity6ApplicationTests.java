fab874d5d510f3ea9c2c3605d580f9674adce3e9
byMagg <byMag@hotmail.es>
2023-04-07 13:31:25 +0200

    update

---
+++ activity-6/src/test/java/dwsc/activity6/Activity6ApplicationTests.java
@@ -0,0 +1,13 @@
+package dwsc.activity6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
