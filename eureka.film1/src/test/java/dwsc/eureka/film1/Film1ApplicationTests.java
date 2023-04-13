cbe63a62580127148e0357325d5a842bfaeff11e
byMagg <byMag@hotmail.es>
2023-04-13 22:05:49 +0200

    add eureka activity7

---
+++ eureka.film1/src/test/java/dwsc/eureka/film1/Film1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dwsc.eureka.film1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Film1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
