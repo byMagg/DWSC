3a85427708b1307a91d1361f0b6cb3a861672c5f
byMagg <byMag@hotmail.es>
2023-04-13 22:22:16 +0200

    update

--- eureka.film1/src/main/java/dwsc/eureka/film1/Film1Application.java
+++ eureka.film1/src/main/java/dwsc/eureka/film1/Film1Application.java
@@ -2,8 +2,10 @@ package dwsc.eureka.film1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Film1Application {
 
 	public static void main(String[] args) {
