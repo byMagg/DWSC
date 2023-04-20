ad03df3f23c374e908e7535682c9d01c7700e006
byMagg <byMag@hotmail.es>
2023-04-20 18:31:09 +0200

    fix

--- activity-4-swagger/src/main/java/dwsc/activity4swagger/controller/UserController.java
+++ activity-4-swagger/src/main/java/dwsc/activity4swagger/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +27,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
-@RestController
+@Controller
 public class UserController {
 
 	@Autowired
