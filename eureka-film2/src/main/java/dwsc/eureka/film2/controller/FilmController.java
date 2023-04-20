2ac465b5ec9dfa93be86616705c35f036e448014
byMagg <byMag@hotmail.es>
2023-04-20 23:27:46 +0200

    update

--- eureka-film2/src/main/java/dwsc/eureka/film2/controller/FilmController.java
+++ eureka-film2/src/main/java/dwsc/eureka/film2/controller/FilmController.java
@@ -6,7 +6,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,16 +21,20 @@ public class FilmController {
 	public ResponseEntity<Boolean> getFilm(@PathVariable String film) {
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create("https://api.themoviedb.org/3/search/movie?api_key=7c88c4114677a284e792acd53302efb1&query="+ film)).build();
+				.uri(URI.create(
+						"https://api.themoviedb.org/3/search/movie?api_key=7c88c4114677a284e792acd53302efb1&query="
+								+ film))
+				.build();
 
 		ObjectMapper mapper = new ObjectMapper();
-		
+
 		String response = "";
 		try {
 			response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
 			JsonNode json = mapper.readTree(response);
 			System.out.println(json.get("total_results").asText());
-			if(json.get("total_results").asText().equals("0")) return new ResponseEntity<Boolean>(false, HttpStatus.OK);
+			if (json.get("total_results").asText().equals("0"))
+				return new ResponseEntity<Boolean>(false, HttpStatus.OK);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
