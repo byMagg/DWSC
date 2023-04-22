f472e35d4a1268bcb9aaa8f4042940fc5fee4ffe
byMagg <byMag@hotmail.es>
2023-04-22 11:42:15 +0200

    update

--- trackinsert/src/main/java/dwsc/trackinsert/TrackInsertApplication.java
+++ trackinsert/src/main/java/dwsc/trackinsert/TrackInsertApplication.java
@@ -1,4 +1,4 @@
-package dwsc.trackinsert;
+package src.main.java.dwsc.trackinsert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
