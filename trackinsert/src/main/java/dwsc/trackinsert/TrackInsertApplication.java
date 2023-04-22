39c726870fe37cdebbc8a657e2885b0ae55a6f95
byMagg <byMag@hotmail.es>
2023-04-22 16:51:37 +0200

    update

--- trackinsert/src/main/java/dwsc/trackinsert/TrackInsertApplication.java
+++ trackinsert/src/main/java/dwsc/trackinsert/TrackInsertApplication.java
@@ -3,9 +3,11 @@ package dwsc.trackinsert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class TrackInsertApplication {
 
 	public static void main(String[] args) {
--- trackinsert/src/main/java/dwsc/trackinsert/dao/TrackCheck.java
+++ trackinsert/src/main/java/dwsc/trackinsert/dao/TrackCheck.java
@@ -1,11 +1,11 @@
-package dwsc.trackcheck.dao;
+package dwsc.trackinsert.dao;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("eureka-film")
-public interface Film {
+@FeignClient("trackcheck")
+public interface TrackCheck {
 	@GetMapping("/films/{film}")
 	public String getFilm(@PathVariable String film);
 }
