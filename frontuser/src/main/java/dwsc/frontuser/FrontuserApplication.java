f3b9b2d322d13de3cdfacabae9fa4c093bdf4cf8
byMagg <byMag@hotmail.es>
2023-04-26 18:07:07 +0200

    updat

---
+++ frontuser/src/main/java/dwsc/frontuser/FrontuserApplication.java
@@ -0,0 +1,13 @@
+package dwsc.frontuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontuserApplication.class, args);
+	}
+
+}
